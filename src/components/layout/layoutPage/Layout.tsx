d054c62b82796937bb8dda5e9c02186f4c021dd9
Shivani Khatri <khatri.shivani96@gmail.com>
2022-07-10 01:34:32 +0200

    dropdown classes moved to headerContainer.scss file

--- src/components/layout/layoutPage/Layout.tsx
+++ src/components/layout/layoutPage/Layout.tsx
@@ -2,19 +2,12 @@ import React, { FC } from "react";
 import Header from "../header/Header";
 import Sidebar from "../Sidebar";
 import "../layoutPage/layoutPage.scss";
-import { faGear, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
-import { useAppSelector, useAppDispatch } from "../../../app/hooks";
-import { toggleActive } from "../header/headerSlice";
+
 type Props = {
   children?: React.ReactNode;
 };
 
 const Layout: FC<Props> = ({ children }) => {
-  const active = useAppSelector((state) => state.header.active);
-  const dispatch = useAppDispatch();
-
   return (
     <div>
       <Header />
