12d81f278b51064051745a3e83b081ffee02b01b
Shivani Khatri <khatri.shivani96@gmail.com>
2022-07-10 17:01:25 +0200

    layout structure added using grid

--- src/components/layout/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sidebar = () => {
-  return (
-    <div>Sidebar will be added here</div>
-  )
-}
-
-export default Sidebar
---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../sidebar/sidebarContainer.scss"
+const Sidebar = () => {
+  return (
+    <div className='container'>Sidebar will be added here</div>
+  )
+}
+
+export default Sidebar
