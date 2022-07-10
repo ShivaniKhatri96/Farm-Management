70a40717e3db46f07af37cdacc63c84ec5c4b209
Shivani Khatri <khatri.shivani96@gmail.com>
2022-07-10 18:39:47 +0200

    searchbar layout created

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import "../sidebar/sidebarContainer.scss"
+import React from "react";
+import "../sidebar/sidebarContainer.scss";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Sidebar = () => {
   return (
-    <div className='container'>Sidebar will be added here</div>
-  )
-}
+    <div className="container">
+      {/* searchbar */}
+      <div className="searchbar-container">
+        <input
+          className="searchbar-styles"
+          type="text"
+          placeholder="Search..."
+        />
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="searchbar-icon" />
+      </div>
 
-export default Sidebar
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+};
+
+export default Sidebar;
