8edc2e570a71e66331eb411b289688da9a1a0e54
Shivani Khatri <khatri.shivani96@gmail.com>
2022-08-08 14:36:14 +0200

    stable table style updated

--- src/components/stableTable/StableTable.tsx
+++ src/components/stableTable/StableTable.tsx
@@ -1,7 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
- faEdit
-} from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import Checkbox from "../miniComponents/Checkbox";
 import "./stableTable.scss";
@@ -13,32 +11,38 @@ const StableTable = () => {
     { name: "Groups" },
   ];
   const tableContent = [
-    { stable: "Stable1", location: "location1", group: "Group1" },
-    { stable: "Stable2", location: "location2", group: "Group2" },
-    { stable: "Stable3", location: "location3", group: "Group3" },
-    { stable: "Stable4", location: "location4", group: "Group4" },
-    { stable: "Stable5", location: "location5", group: "Group5" },
+    { id: 1, stable: "Stable1", location: "location1", group: "1" },
+    { id: 2, stable: "Stable2", location: "location2", group: "2" },
+    { id: 3, stable: "Stable3", location: "location3", group: "3" },
+    { id: 4, stable: "Stable4", location: "location4", group: "4" },
+    { id: 5, stable: "Stable5", location: "location5", group: "5" },
   ];
 
   return (
     <div className="tableFlex">
       <div className="tableBox">
-        <div>Stables</div>
+        <div className="title">Stables</div>
         <div className="innerBox">
           <div className="gridRow">
             <Checkbox />
             {tableHeading.map((heading) => (
-              <div>{heading.name}</div>
+              <div key={heading.name}>{heading.name}</div>
             ))}
           </div>
 
           {tableContent.map((content) => (
-            <div className="gridRow">
+            //checking if the last id of the tableContent is same as content id
+            <div
+              className={
+                tableContent[tableContent.length - 1].id === content.id ? "gridRowNoBorder" : "gridRowWithBorder"
+              }
+              key={content.id}
+            >
               <Checkbox />
               <div>{content.stable}</div>
               <div>{content.location}</div>
               <div>{content.group}</div>
-              <FontAwesomeIcon icon={faEdit} />
+              <FontAwesomeIcon icon={faEdit} className="editIcon" />
             </div>
           ))}
         </div>
